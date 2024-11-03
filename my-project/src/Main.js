55f1a456470b55aee3eb735a1817086993a126fc
NagaMaruthi <nagamaruthi567@gmail.com>
2024-11-03 12:07:45 +0530

    first-commit

--- my-project/src/Main.js
+++ my-project/src/Main.js
@@ -4,31 +4,35 @@ class Main extends React.Component{
         super();
         this.state={
             username:"",
-            usermail:"",
+            useremail:"",
             userpassword:""
         };
     };
     getdata=((event)=>{
         console.log(event.target.value);
-        var value=event.target.value
-        var username=event.target.username
-        this.setState({[username]:value})
+        var value=event.target.value;
+        var name=event.target.name;
+        this.setState({[name]:value});
     });
-    abc=(()=>{
+    abc=((event)=>{
+            event.preventDefault();
         alert("hi");
-        console.log(this.state)
+        console.log(this.state);
 
     })
     render(){
         return<div>
+            <form>
             <input placeholder="Enter User Name" type="text" onChange={this.getdata} name="username"></input>
             <br></br>
-            <input placeholder="Enter User Email" type="text" onChange={this.getdata} name="useremial"></input>
+            <input placeholder="Enter User Email" type="text" onChange={this.getdata} name="useremail"></input>
             <br></br>
             <input placeholder="Enter User Password" onChange={this.getdata} type="text" name="userpassword"></input>
             <br></br>
             <button onClick={this.abc}>submit</button>
+            </form>
+            
         </div>
     }
-}
+}    
 export default Main;
