22e62c5e8d95991bde96a2912f7e4bd5c8cf59ae
NagaMaruthi <nagamaruthi567@gmail.com>
2024-11-29 06:04:02 +0530

    first-commit

---
+++ my-project/src/my.js
@@ -0,0 +1,10 @@
+import React from "react"
+export default function My(){
+    return(
+        <React.Fragment>
+            <h1>hello</h1>
+            <p>world!</p>
+        </React.Fragment>
+        
+    )
+}
---
+++ my-project/src/mycomponent.js
@@ -0,0 +1,9 @@
+import React from "react"
+export default function MyComponent(){
+    return(
+        <>
+        <h1>hello</h1>
+        <p>world</p>
+        </>
+    )
+}
