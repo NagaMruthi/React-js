6992c79fad64616c3f0037bac1478127610176e8
NagaMaruthi <nagamaruthi567@gmail.com>
2024-12-24 18:17:04 +0530

    commit

---
+++ my-project/src/hook.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useEffect } from "react";
+function Hook(){
+    useEffect(()=>{
+        var x=10;
+        setInterval(() => {
+            console.log(x)
+            x++;
+            if(x===20){
+           clearInterval();
+            }
+           
+        },100);
+        return clearInterval;
+    })
+    return(
+        <React.Fragment>
+            <h1>hook</h1>
+        </React.Fragment>
+    )
+}
+export default Hook;
